9fc1bb20059e7381b55d8d0cca7dc1e0a97cd650
Hammerill <www.hammerill@gmail.com>
2023-09-29 15:30:52 +0200

    Enhance run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-echo "Starting a container... Try connecting to the SSH at port 2222 in another terminal (ssh test@localhost -p 2222 with password 'changeme')."
-docker run -p 2222:22 hammerill/myubuntu-ssh:v1.0.0 > /dev/null
+echo "Starting a container... Try connecting to the SSH at port 2222 (ssh test@localhost -p2222 with password 'changeme')."
+docker run -p 2222:22 hammerill/myubuntu-ssh:v1.0.0 > /dev/null &
